834577db32293fea9217238ab286bf29508d3125
tahaxweb <tancilingir@gmail.com>
2025-01-04 18:42:11 +0300

    add pwa

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,10 @@
-import type { NextConfig } from "next";
-const withPWA = require('next-pwa');
-
-module.exports = withPWA({
-  pwa: {
-    dest: 'public', // PWA dosyalarının oluşturulacağı dizin
-    disable: process.env.NODE_ENV === 'development', // Geliştirme modunda PWA'yı devre dışı bırak
-  },
-});
-
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+/** @type {import('next').NextConfig} */
+const withPWA = require('next-pwa')({
+  dest: 'public'
+  });
+  
+  module.exports = withPWA({
+    // next.js config
+  })
+  
+  
