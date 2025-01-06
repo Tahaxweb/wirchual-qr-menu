a062c68af7e3e76003e85d80dfc4507430a96fa0
tahaxweb <tancilingir@gmail.com>
2025-01-06 16:52:58 +0300

    add usePathname at sidebar.tsx

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -1,47 +1,55 @@
-"use client"
-import { Fragment, useState } from 'react'
-import { Dialog, Transition } from '@headlessui/react'
-import { RiApps2Line, RiCloseLine, RiFolder2Line, RiHome6Line, RiLogoutBoxLine, RiMenuLine, RiPriceTag3Line } from '@remixicon/react'
-import * as Dropdown from '@/components/ui/dropdown';
-import Image from 'next/image'
+"use client";
 
-import { useRouter } from 'next/navigation';
+import { Fragment, useState } from "react";
+import { Dialog, Transition } from "@headlessui/react";
+import {
+  RiApps2Line,
+  RiCloseLine,
+  RiFolder2Line,
+  RiHome6Line,
+  RiLogoutBoxLine,
+  RiMenuLine,
+  RiPriceTag3Line,
+} from "@remixicon/react";
+import * as Dropdown from "@/components/ui/dropdown";
+import Image from "next/image";
+import { useRouter , usePathname } from "next/navigation";
 
 const navigation = [
-  { name: 'Dashboard', href: '#', icon: RiHome6Line, current: true },
-  { name: 'Kategoriler', href: '/admin/categorys', icon: RiFolder2Line, current: false },
-  { name: 'Ürünler', href: '#', icon: RiPriceTag3Line, current: false },
-  { name: 'Eklentiler', href: '#', icon:RiApps2Line ,current: false },
-]
+  { name: "Dashboard", href: "/admin/dashboard", icon: RiHome6Line, current: true },
+  { name: "Kategoriler", href: "/admin/categorys", icon: RiFolder2Line, current: false },
+  { name: "Ürünler", href: "#", icon: RiPriceTag3Line, current: false },
+  { name: "Eklentiler", href: "#", icon: RiApps2Line, current: false },
+];
 
-function classNames(...classes : string[]) {
-  return classes.filter(Boolean).join(' ')
+function classNames(...classes: string[]) {
+  return classes.filter(Boolean).join(" ");
 }
 
-export default function Sidebar({children}: {children: React.ReactNode}) {
-  const [sidebarOpen, setSidebarOpen] = useState(false)
+export default function Sidebar({ children }: { children: React.ReactNode }) {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
   const router = useRouter();
-
+  const pathname = usePathname();
+ 
   const handleLogout = async (): Promise<void> => {
     try {
-      const response = await fetch('/api/logout', {
-        method: 'GET',
+      const response = await fetch("/api/logout", {
+        method: "GET",
       });
 
       if (response.ok) {
         // Logout başarılı, giriş sayfasına yönlendir
-        router.push('/login');
+        router.push("/login");
       } else {
-        console.error('Logout failed');
+        console.error("Logout failed");
       }
     } catch (error) {
-      console.error('Error during logout:', error);
+      console.error("Error during logout:", error);
     }
   };
 
   return (
     <>
-    
       <div>
         <Transition.Root show={sidebarOpen} as={Fragment}>
           <Dialog as="div" className="relative z-50 lg:hidden" onClose={setSidebarOpen}>
@@ -78,9 +86,13 @@ export default function Sidebar({children}: {children: React.ReactNode}) {
                     leaveTo="opacity-0"
                   >
                     <div className="absolute left-full top-0 flex w-16 justify-center pt-5">
-                      <button type="button" className="-m-2.5 p-2.5" onClick={() => setSidebarOpen(false)}>
+                      <button
+                        type="button"
+                        className="-m-2.5 p-2.5"
+                        onClick={() => setSidebarOpen(false)}
+                      >
                         <span className="sr-only">Close sidebar</span>
-                       <RiCloseLine className="h-6 w-6 shrink-0 text-white" aria-hidden="true" />
+                        <RiCloseLine className="h-6 w-6 shrink-0 text-white" aria-hidden="true" />
                       </button>
                     </div>
                   </Transition.Child>
@@ -94,29 +106,32 @@ export default function Sidebar({children}: {children: React.ReactNode}) {
                         width={124}
                         height={124}
                       />
-                     <span className=' text-paragraph-lg  font-semibold text-white'> Wirchual QR Menu</span>
+                      <span className=" text-paragraph-lg  font-semibold text-white">
+                        Wirchual QR Menu
+                      </span>
                     </div>
                     <nav className="flex flex-1 flex-col">
-                      <ul role="list" className="-mx-2 flex-1 space-y-1">
+                    <ul role="list" className="-mx-2 flex-1 space-y-1">
                         {navigation.map((item) => (
                           <li key={item.name}>
                             <a
                               href={item.href}
                               className={classNames(
-                                item.current
-                                  ? 'bg-neutral-600 text-white'
-                                  : 'text-gray-400 hover:text-white hover:bg-neutral-700',
-                                'group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold'
+                                pathname === item.href
+                                  ? "bg-neutral-600 text-white"
+                                  : "text-gray-400 hover:text-white hover:bg-neutral-700",
+                                "group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold"
                               )}
                             >
-                              <item.icon className="h-6 w-6 shrink-0" aria-hidden="true" />
+                              <item.icon
+                                className="h-6 w-6 shrink-0"
+                                aria-hidden="true"
+                              />
                               {item.name}
-                              
                             </a>
                           </li>
                         ))}
                       </ul>
-                      
                     </nav>
                   </div>
                 </Dialog.Panel>
@@ -136,67 +151,84 @@ export default function Sidebar({children}: {children: React.ReactNode}) {
               height={64}
             />
           </div>
-            <div className="border-b border-neutral-500 mx-auto my-3   w-16"></div> 
-          <nav className="">
-            <ul role="list" className="flex flex-col items-center space-y-1">
+          <div className="border-b border-neutral-500 mx-auto my-3   w-16"></div>
+          <nav>
+          <ul role="list" className="flex flex-col items-center space-y-1">
               {navigation.map((item) => (
                 <li key={item.name}>
                   <a
                     href={item.href}
                     className={classNames(
-                      item.current ? 'bg-neutral-600 text-white' : 'text-white hover:text-white ',
-                      'group flex   text-[10px] max-w-14 rounded-lg min-h-14 px-2 items-center max-h-16  justify-center gap-1 flex-col'
+                      pathname === item.href
+                        ? "bg-neutral-600 text-white"
+                        : "text-white hover:text-white",
+                      "group flex text-[10px] max-w-14 rounded-lg min-h-14 px-2 items-center max-h-16 justify-center gap-1 flex-col"
                     )}
                   >
-                    <item.icon className="h-6 w-6 shrink-0" aria-hidden="true" />
-                    <span className=" ">{item.name}</span>
+                    <item.icon
+                      className="h-6 w-6 shrink-0"
+                      aria-hidden="true"
+                    />
+                    <span>{item.name}</span>
                   </a>
                 </li>
               ))}
-            </ul>   
+            </ul>
           </nav>
         </div>
 
         <div className="sticky top-0 z-40 flex items-center justify-between gap-x-6 bg-neutral-800 px-4 py-4 shadow-sm sm:px-6 lg:hidden">
-        <button type="button" className="-m-2.5 p-2.5 text-gray-400 lg:hidden" onClick={() => setSidebarOpen(true)}>
+          <button
+            type="button"
+            className="-m-2.5 p-2.5 text-gray-400 lg:hidden"
+            onClick={() => setSidebarOpen(true)}
+          >
             <span className="sr-only">Open sidebar</span>
             <RiMenuLine className="h-6 w-6 shrink-0 text-white" aria-hidden="true" />
           </button>
           <Dropdown.Root>
-          <Dropdown.Trigger asChild>
-            <img
-              className="h-10 w-10 rounded-full bg-neutral-800"
-              src={"/icons/avatar.svg"}
-              alt=""
-            />
+            <Dropdown.Trigger asChild>
+              <img
+                className="h-10 w-10 rounded-full bg-neutral-800"
+                src={"/icons/avatar.svg"}
+                alt=""
+              />
             </Dropdown.Trigger>
-            <Dropdown.Content align='start' className=' mr-2 mt-2'>
-              <div onClick={handleLogout} className='py-2.5 hover:bg-neutral-100 hover:border  text-label-md flex gap-1  rounded-10 px-4'> <RiLogoutBoxLine/> Log out </div>
-              </Dropdown.Content>
-            </Dropdown.Root>
+            <Dropdown.Content align="start" className="mr-2 mt-2">
+              <div
+                onClick={handleLogout}
+                className="py-2.5 hover:bg-neutral-100 hover:border  text-label-md flex gap-1  rounded-10 px-4"
+              >
+                <RiLogoutBoxLine /> Log out
+              </div>
+            </Dropdown.Content>
+          </Dropdown.Root>
         </div>
-        
+
         <main className="lg:pl-24 pr-4">
-        <div className=' justify-end lg:flex  items-center  w-full hidden px-5 pt.2.5 lg:justify-end lg:block border-b h-14'>
-       
-          <Dropdown.Root>
-          <Dropdown.Trigger asChild>
-            <img
-              className="h-10 w-10 rounded-full bg-neutral-800"
-              src={"/icons/avatar.svg"}
-              alt=""
-            />
-            </Dropdown.Trigger>
-            <Dropdown.Content align='start' className=' mr-2 mt-2'>
-              <div onClick={handleLogout} className='py-2.5 hover:bg-neutral-100 hover:border  text-label-md flex gap-1  rounded-10 px-4'> <RiLogoutBoxLine/> Log out </div>
+          <div className="justify-end lg:flex  items-center  w-full hidden px-5 pt.2.5 lg:justify-end lg:block border-b h-14">
+            <Dropdown.Root>
+              <Dropdown.Trigger asChild>
+                <img
+                  className="h-10 w-10 rounded-full bg-neutral-800"
+                  src={"/icons/avatar.svg"}
+                  alt=""
+                />
+              </Dropdown.Trigger>
+              <Dropdown.Content align="start" className="mr-2 mt-2">
+                <div
+                  onClick={handleLogout}
+                  className="py-2.5 hover:bg-neutral-100 hover:border  text-label-md flex gap-1  rounded-10 px-4"
+                >
+                  <RiLogoutBoxLine /> Log out
+                </div>
               </Dropdown.Content>
             </Dropdown.Root>
-        </div>
- 
-             
-            {children}
+          </div>
+
+          {children}
         </main>
       </div>
     </>
-  )
+  );
 }
